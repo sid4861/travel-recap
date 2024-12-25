ef78a1405b4d95bde83708e66f9306be8229b39d
Siddharth <siddharthlodha42@gmail.com>
2024-12-25 20:50:48 +0530

    as

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-"use client"
-import TravelRecap from "@/components/TravelRecap";
+"use client";
+// import TravelRecap from "@/components/TravelRecap";
+
+import dynamic from "next/dynamic";
+
+const TravelRecapWithNoSSR = dynamic(
+  () => import("../components/TravelRecap"),
+  { ssr: false }
+);
 
 export default function Home() {
-  return <TravelRecap />;
+  return <TravelRecapWithNoSSR />;
 }
